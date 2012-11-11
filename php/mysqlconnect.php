479ae8911e1d3b13877260d07897115a33f6a8b0
Jérémie Rappaz <jeremie.rappaz@epfl.ch>
2012-11-11 20:51:54 +0100

    initial commit

---
+++ php/mysqlconnect.php
@@ -0,0 +1,4 @@
+<?php
+$connexion = mysql_connect("localhost","root","free-j") or die("Unable to connect database");
+mysql_select_db("mesencore", $connexion) or die("Unable to select database");
+?>
